22f36b81fab659ed72e3e60f8d500f0b36462dd9
bimnova001 <160095853+bimnova001@users.noreply.github.com>
2025-04-08 13:48:54 +0700

    Update video.py

--- video.py
+++ video.py
@@ -4,12 +4,46 @@ import time
 import tempfile
 import random
 import time
+import tkinter as tk
+import threading
 # ASCII แสดงจาก pixel
 ASCII_CHARS = "@%#*+=-:. "
 WIDTH, HEIGHT = 120, 40
 
 
+def show_random_popup(title, message):
+    def popup():
+        root = tk.Tk()
+        root.withdraw()  
 
+
+        win = tk.Toplevel()
+        win.title(title)
+
+
+        win_width, win_height = 300, 100
+
+        screen_width = win.winfo_screenwidth()
+        screen_height = win.winfo_screenheight()
+
+        x = random.randint(0, screen_width - win_width)
+        y = random.randint(0, screen_height - win_height)
+
+        win.geometry(f"{win_width}x{win_height}+{x}+{y}")
+
+        label = tk.Label(win, text=message, font=("Arial", 16))
+        label.pack(expand=True)
+
+        btn = tk.Button(win, text="OK", command=win.destroy)
+        btn.pack(pady=5)
+
+        win.mainloop()
+
+    threading.Thread(target=popup).start()
+
+def show_warning():
+    show_random_popup("Warning", "LOL")
+    show_random_popup("Error", "LOL")
 
 def frame_to_ascii(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -39,6 +73,7 @@ def play_ascii_video_from_bin(bin_path):
         ascii_frame = frame_to_ascii(frame)
         os.system("cls" if os.name == "nt" else "clear")
         print(ascii_frame)
+        threading.Thread(target=show_warning).start()
         time.sleep(0.03)
     cap.release()
     
